d07a3ec5a8cd42ded2f7bd1731b713ff3617f7d9
merbayatte <72632880+merbayatte@users.noreply.github.com>
2020-11-04 23:08:48 -0800

    Update count_up.py

--- count_up.py
+++ count_up.py
@@ -1,15 +1,10 @@
 #!/usr/bin/env python3
 
-# this is a python script template
-# this next line will download the file using curl
-
 gff="Escherichia_coli_str_k_12_substr_mg1655.ASM584v2.37.gff3.gz"
 fasta="Escherichia_coli_str_k_12_substr_mg1655.ASM584v2.dna.chromosome.Chromosome.fa.gz"
 
 import os,gzip,itertools,csv,re
 
-# this is code which will parse FASTA files
-# define what a header looks like in FASTA format
 def isheader(line):
     return line[0] == '>'
 
@@ -24,8 +19,6 @@ def aspairs(f):
             sequence = ''.join(line.strip() for line in group)
             yield seq_id, sequence
 
-
-
 if not os.path.exists(gff):
     os.system("curl -O ftp://ftp.ensemblgenomes.org/pub/bacteria/release-45/gff3/bacteria_0_collection/escherichia_coli_str_k_12_substr_mg1655/Escherichia_coli_str_k_12_substr_mg1655.ASM584v2.37.gff3.gz")
 
@@ -33,17 +26,21 @@ if not os.path.exists(fasta):
     os.system("curl -O ftp://ftp.ensemblgenomes.org/pub/bacteria/release-45/fasta/bacteria_0_collection/escherichia_coli_str_k_12_substr_mg1655/dna/Escherichia_coli_str_k_12_substr_mg1655.ASM584v2.dna.chromosome.Chromosome.fa.gz")
     
 with gzip.open(gff,"rt") as fh:
-    # now add code to process this
     gff = csv.reader(fh,delimiter="\t")
-   gene_count = 0
+    gene_count = 0
 
     for row in gff:
-
         if row[0].startswith("#"):
             continue
-
         if "gene" == row[2]:
-
             gene_count += 1
-            
-print(gene_count)
+    print("total number of genes is",gene_count)
+
+    total_genelen = 0
+    for row in gff:
+        if row[0].startswith("#"):
+            continue 
+        if "gene"==row[2]: 
+            genelen = int(row[5])-int(row[4])
+            total_genlen += genlen
+    print("total gene length is",total_genelen)
